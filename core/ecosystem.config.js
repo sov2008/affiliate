c1e8b1dc9da1a3e91917ee431732d81f62df3e1f
sov2008 <sapegin.oleg@gmail.com>
2026-09-13 13:49:03 +0300

    feat(pm2): include affiliate-telegram-userbot in core ecosystem config

--- core/ecosystem.config.js
+++ core/ecosystem.config.js
@@ -109,6 +109,23 @@ module.exports = {
       }
     },
     {
+      name: "affiliate-telegram-userbot",
+      script: "./dist/services/telegram-userbot.service.js",
+      cwd: __dirname,
+      instances: 1,
+      autorestart: true,
+      max_memory_restart: "100M",
+      restart_delay: 5000,
+      exp_backoff_restart_delay: 500,
+      out_file: path.join(logsDir, "pm2-affiliate-telegram-userbot-out.log"),
+      error_file: path.join(logsDir, "pm2-affiliate-telegram-userbot-error.log"),
+      merge_logs: true,
+      time: true,
+      env: {
+        NODE_ENV: "production"
+      }
+    },
+    {
       name: "affiliate-social-syndicator",
       script: "./dist/workers/social-syndicator.worker.js",
       cwd: __dirname,
